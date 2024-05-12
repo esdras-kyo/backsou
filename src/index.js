32bddc0f9843edef599c64475d1977f8d24197e6
edas <esdrascamel@gmail.com>
2024-05-12 08:13:37 -0300

    ofic

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import cors from 'cors'
 import {Mercado_Pago} from './router/MC_Router.js'
 import { google} from "googleapis"
 import dotenv from 'dotenv'
-import functions from "firebase-functions"
 
 dotenv.config()
 
